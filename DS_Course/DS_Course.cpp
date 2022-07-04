99e1ec1a47d51966549e646b5f6e4a117c4d0958
Abdelrahman <101744741+AbdelrahmanMoh-Hussain@users.noreply.github.com>
2022-07-04 14:53:28 +0200

    Node class

--- DS_Course/DS_Course.cpp
+++ DS_Course/DS_Course.cpp
@@ -3,18 +3,71 @@
 
 #include <iostream>
 #include "Vector.h"
+#include "LinkedList.h"
 using namespace std;
+
+
+class Node {
+public:
+	int data;
+	Node* next = nullptr;
+
+	Node(int data) : data(data) {}
+	void static print(Node* head) {
+		if (head == nullptr)
+		{
+			cout << endl;
+			return;
+		}
+		cout << head->data << ' ';
+		head = head->next;
+		print(head);
+	}
+	void static printReversed(Node* head) {
+		if (head == nullptr)
+		{
+			return;
+		}
+		printReversed(head->next);
+		cout << head->data << ' ';
+	}
+	void static print1(Node* head) {
+		for (; head != nullptr; head = head->next)
+		{
+			cout << head->data << ' ';
+		}
+		cout << endl;
+	}
+};
+
+Node* find(int val, Node* head) {
+	while (head != nullptr) {
+		if (head->data == val)
+		{
+			return head;
+		}
+		head = head->next;
+	}
+	return nullptr;
+}
 
 int main()
 {
-	Vector v(10);
-	v.push_back(3);
-	v.push_back(1);
-	v.push_back(2);
-	v.insert(0, 5);
-	//cout << "removed item: " << v.remove(1) << endl;
-	//cout << "Position is : " << v.find_transposition(2) << endl;
-	v.right_rotation(2);
+	Node* node1 = new Node(6);
+	Node* node2 = new Node(10);
+	Node* node3 = new Node(8);
+	Node* node4 = new Node(15);
 
-	v.print();
+	node1->next = node2;
+	node2->next = node3;
+	node3->next = node4;
+	node4->next = nullptr;
+
+	Node::print(node1);
+	cout << "---------------------" << endl;
+	Node::printReversed(node1);
+	cout << "\n---------------------" << endl;
+	Node::print1(node1);
+
+	
 }
